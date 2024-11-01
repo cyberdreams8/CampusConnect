a38d49fb130149c4494467e980804c54f088feba
devansh-12 <devansh.unofficial2345@gmail.com>
2024-11-01 14:32:43 +0530

    add few pages / node setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './public/pages/**/*.html', // Include all HTML files within subdirectories of pages
+    './public/pages/**/*.js',   // If you are using JavaScript files with Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
