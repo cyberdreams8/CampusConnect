36f5d2b80e700f3c4c38ba990c13b68a9ed4269b
cyberdreams8 <nethra.nayak23@spit.ac.in>
2024-11-07 21:18:28 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,26 @@
 const express = require('express');
 const router = express.Router();
 
+
+const { isAuthenticated, isRecruiter, isStudent, isAdmin } = require('../middleware/authMiddleware');
+
+// Route for recruiter-only actions
+router.get('/recruiter/dashboard', isAuthenticated, isRecruiter, (req, res) => {
+    res.send('Welcome to the recruiter dashboard');
+});
+
+// Route for student-only actions
+router.get('/student/dashboard', isAuthenticated, isStudent, (req, res) => {
+    res.send('Welcome to the student dashboard');
+});
+
+// Route for admin-only actions
+router.get('/admin/dashboard', isAuthenticated, isAdmin, (req, res) => {
+    res.send('Welcome to the admin dashboard');
+});
+
+
+
 router.post('/logout', (req, res) => {
     req.session.destroy((err) => {
         if (err) {
